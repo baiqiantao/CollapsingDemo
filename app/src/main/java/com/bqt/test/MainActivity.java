9357211bc2212f7eae61cb91fa22db46a6b31838
baiqiantao <baiqiantao@sina.com>
2018-05-10 18:28:43 +0800

    init

--- app/src/main/java/com/bqt/test/MainActivity.java
+++ app/src/main/java/com/bqt/test/MainActivity.java
@@ -1,38 +1,14 @@
 package com.bqt.test;
 
-import android.app.ListActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-
-public class MainActivity extends ListActivity {
+public class MainActivity extends Activity {
 	
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		String[] array = {"",
-				"",
-				"",
-				"",
-				"",};
-		setListAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, new ArrayList<>(Arrays.asList(array))));
-	}
-	
-	@Override
-	protected void onListItemClick(ListView l, View v, int position, long id) {
-		switch (position) {
-			case 0:
-				
-				break;
-			case 1:
-				
-				break;
-			case 2:
-				
-				break;
-		}
+		setContentView(R.layout.first_activity);
+		findViewById(R.id.tv).setOnClickListener(v -> startActivity(new Intent(MainActivity.this, SecondActivity.class)));
 	}
 }
---
+++ app/src/main/java/com/bqt/test/SecondActivity.java
@@ -0,0 +1,12 @@
+package com.bqt.test;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SecondActivity extends Activity {
+	
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.second_activity);
+	}
+}
